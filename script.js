8d11d4c27feceb1c87b76cf266e02619ff696306
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-03-18 10:07:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function verificarPar(numero) {
+    if (numero % 2 === 0) {
+        console.log("par");
+    }
+    else{
+        console.log("impar");
+    }
+}
+verificarPar(1)
+
+function calcular(n1, n2){
+    console.log(n1+n2);
+    console.log(n1-n2);
+    console.log(n1*n2);
+    console.log(n1/n2);
+}
+calcular(4, 2)
+
+let i = 10;
+while (i>=1) {
+    console.log(i);
+    i--;
+}
+
+function inverterTexto(str){
+    
+}
