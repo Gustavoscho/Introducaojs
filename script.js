e37e27053b541b0698ca2388c4402aa890ed2c9c
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-03-20 12:57:05 -0300

    6, 7, 8

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ function verificarPar(numero) {
     if (numero % 2 === 0) {
         console.log("par");
     }
-    else{
+    else {
         console.log("impar");
     }
 }
 verificarPar(1)
 
-function calcular(n1, n2){
-    console.log(n1+n2);
-    console.log(n1-n2);
-    console.log(n1*n2);
-    console.log(n1/n2);
+function calcular(n1, n2) {
+    console.log(n1 + n2);
+    console.log(n1 - n2);
+    console.log(n1 * n2);
+    console.log(n1 / n2);
 }
 calcular(4, 2);
 
 let i = 10;
-while (i>=1) {
+while (i >= 1) {
     console.log(i);
     i--;
 }
 
-function inverterTexto(str){
-    str = str.split(""); 
-    str = str.reverse(); 
-    str = str.join(""); 
+function inverterTexto(str) {
+    str = str.split("");
+    str = str.reverse();
+    str = str.join("");
     console.log(str);
 }
 inverterTexto("bom dia");
@@ -35,12 +35,42 @@ function contarCaracteres(str) {
 }
 contarCaracteres("animal")
 
-var carro = new Object();
-carro.marca = "Fiat";
-carro.modelo = "uno";
-carro.ano = "2010";
+const carro = {
+    marca: "Fiat",
+    modelo: "uno",
+    ano: "2010",
+};
 console.log(carro.modelo);
 
-function mensagemPersonalizada (nome, mensagem){
-    
+function mensagemPersonalizada(nome, msg) {
+    console.log(nome + msg);
 }
+mensagemPersonalizada("gustavo ", "olá");
+
+function media(n1, n2, n3){
+    console.log((n1+n2+n3)/3)
+}
+media(4,5,8);
+
+for(multiplo=1; multiplo<=20; multiplo++){
+    if (multiplo % 3 == 0)
+    {
+       console.log(multiplo + " multiplo de 3");
+    }else{
+        console.log(multiplo + " não é multiplo de 3");
+    }
+}
+
+function verificarPalindromo(str){
+    let str2 = str
+    str = str.split("");
+    str = str.reverse();
+    str = str.join("");
+    if (str = str2)
+    {
+       console.log("palindromo verdadeiro");
+    }else{
+        console.log("palindromo falso");
+    }
+}
+verificarPalindromo(arara);
