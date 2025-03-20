e66abfc08d6349574bf44e54b168b0de6003e73c
gustavoscho <gustavo_hoepfner@estudante.sesisenai.org.br>
2025-03-20 12:10:58 -0300

    exerc. 4, 5, 6

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function calcular(n1, n2){
     console.log(n1*n2);
     console.log(n1/n2);
 }
-calcular(4, 2)
+calcular(4, 2);
 
 let i = 10;
 while (i>=1) {
@@ -23,5 +23,24 @@ while (i>=1) {
 }
 
 function inverterTexto(str){
+    str = str.split(""); 
+    str = str.reverse(); 
+    str = str.join(""); 
+    console.log(str);
+}
+inverterTexto("bom dia");
+
+function contarCaracteres(str) {
+    console.log(str.length)
+}
+contarCaracteres("animal")
+
+var carro = new Object();
+carro.marca = "Fiat";
+carro.modelo = "uno";
+carro.ano = "2010";
+console.log(carro.modelo);
+
+function mensagemPersonalizada (nome, mensagem){
     
 }
